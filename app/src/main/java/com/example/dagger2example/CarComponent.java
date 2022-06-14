1c0f6036ab66a484454f35d03e2c8f90e61707a0
Nowocinski <vito24222@gmail.com>
2022-06-14 15:32:03 +0200

    Added injection to classes

---
+++ app/src/main/java/com/example/dagger2example/CarComponent.java
@@ -0,0 +1,8 @@
+package com.example.dagger2example;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
---
+++ app/src/main/java/com/example/dagger2example/Engine.java
@@ -0,0 +1,9 @@
+package com.example.dagger2example;
+
+import javax.inject.Inject;
+
+public class Engine {
+    @Inject
+    public Engine() {
+    }
+}
